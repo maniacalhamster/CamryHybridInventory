5d60d5a845ea81cb2603367242389c5cce43b44c
Akar Singh <aks002@ucsd.edu>
2024-02-11 09:16:36 -0800

    space formatting

--- CamryInventory.psm1
+++ CamryInventory.psm1
@@ -1,26 +1,27 @@
 $rawData = (Get-Content 'data.json' | ConvertFrom-Json)
 
 $dealer = @{
-    Name = 'dealer'
-    Expression = {$_.dealerMarketingName}
+    Name       = 'dealer'
+    Expression = { $_.dealerMarketingName }
 }
 
 $model = @{
-    Name = 'model'
-    Expression = {$_.model.marketingName}
+    Name       = 'model'
+    Expression = { $_.model.marketingName }
 }
 
 $tsrp = @{
-    Name = 'tsrp'
-    Expression = {$_.price.totalMsrp}
+    Name       = 'tsrp'
+    Expression = { $_.price.totalMsrp }
 }
 
 $markup = @{
-    Name = 'markup'
+    Name       = 'markup'
     Expression = {
         if ($_.price.advertizedPrice -gt $_.price.totalMsrp) {
             return $_.price.advertizedPrice - $_.price.totalMsrp
-        } elseif ($_.price.sellingPrice -gt $_.price.totalMsrp) {
+        }
+        elseif ($_.price.sellingPrice -gt $_.price.totalMsrp) {
             return $_.price.sellingPrice - $_.price.totalMsrp
         }
         return $null
@@ -28,27 +29,27 @@ $markup = @{
 }
 
 $seating = @{
-    Name = 'seating'
-    Expression = {$_.intColor.marketingName}
+    Name       = 'seating'
+    Expression = { $_.intColor.marketingName }
 }
 
 $color = @{
-    Name = 'color'
-    Expression = {$_.extColor.marketingName}
+    Name       = 'color'
+    Expression = { $_.extColor.marketingName }
 }
 
 $etaFrom = @{
-    Name = 'etaFrom'
-    Expression = {$_.eta.currFromDate}
+    Name       = 'etaFrom'
+    Expression = { $_.eta.currFromDate }
 }
 
 $etaTo = @{
-    Name = 'etaTo'
-    Expression = {$_.eta.currToDate}
+    Name       = 'etaTo'
+    Expression = { $_.eta.currToDate }
 }
 
 $options = @{
-    Name = 'options'
+    Name       = 'options'
     Expression = {
         $_.options | Sort-Object optionType | Select-Object optionType, optionCd, marketingName, marketingLongName
     }
@@ -74,7 +75,7 @@ function Get-DealerInventory {
         [string[]]
         $model,
 
-        [ValidateSet("BD","CP","D5","EF","FE","RO","SR","UP","WL","1T","2T","MAX","S5","CY","59","PC","NL","DK","MF","XPEL-PAINT-PROTECTION","WINDOW-TINT","DOOR-SILL-PROTECTOR","WHL-LOCKS","XPEL-PAINT-PROTECTION-DOOR-CUPS-DOOR-EDGE-GUARDS","XPEL-CERAMIC-EXPRESS","UM","CF","G4","TX","KAR","UDA","UDC","YY","74","PURSUITALARM","3MBASIC","TOO-PROTECTION","9G","G0","CLEAR-GUARD-DOOR-EDGE-GUARDS","ELO-GPS-THEFT-PROTECTION","3Z","GE","7R","3P","BK","VPP","IRVINE-PRE-LOAD-DENT","IRVINE-PRE-LOAD-APP","SCT-PROTECTION","GD","V1","FT-0001","AMU3","SURF-PT","SECURITY-GPS-SYSTEM","CLEARPRO-DOOR-PROTECTION","ANTI-THEFT-WHEEL-LOCKS","ENVIRONMENTAL-PKG","XZILON-CERAMIC-COATING","THEFT-PROTECTION","PROPAK","DA","HL","2","WT")]
+        [ValidateSet("BD", "CP", "D5", "EF", "FE", "RO", "SR", "UP", "WL", "1T", "2T", "MAX", "S5", "CY", "59", "PC", "NL", "DK", "MF", "XPEL-PAINT-PROTECTION", "WINDOW-TINT", "DOOR-SILL-PROTECTOR", "WHL-LOCKS", "XPEL-PAINT-PROTECTION-DOOR-CUPS-DOOR-EDGE-GUARDS", "XPEL-CERAMIC-EXPRESS", "UM", "CF", "G4", "TX", "KAR", "UDA", "UDC", "YY", "74", "PURSUITALARM", "3MBASIC", "TOO-PROTECTION", "9G", "G0", "CLEAR-GUARD-DOOR-EDGE-GUARDS", "ELO-GPS-THEFT-PROTECTION", "3Z", "GE", "7R", "3P", "BK", "VPP", "IRVINE-PRE-LOAD-DENT", "IRVINE-PRE-LOAD-APP", "SCT-PROTECTION", "GD", "V1", "FT-0001", "AMU3", "SURF-PT", "SECURITY-GPS-SYSTEM", "CLEARPRO-DOOR-PROTECTION", "ANTI-THEFT-WHEEL-LOCKS", "ENVIRONMENTAL-PKG", "XZILON-CERAMIC-COATING", "THEFT-PROTECTION", "PROPAK", "DA", "HL", "2", "WT")]
         [string[]]
         $options,
 
@@ -118,15 +119,16 @@ function Get-DealerInventory {
     if ($PSCmdlet.MyInvocation.PipelineLength -eq $PSCmdlet.MyInvocation.PipelinePosition) {
         if ($showOptions) {
             $optionPref = @{
-                Name = 'P'
-                Expression = {if ($_.optionCd -in $options) {"*"}}
+                Name       = 'P'
+                Expression = { if ($_.optionCd -in $options) { "*" } }
             }
             $sortPref = @{
-                Expression = {$_.P}
+                Expression = { $_.P }
                 Descending = $true
             }
             $expr += " | % {`$_ | select * -ExcludeProperty options; `$_.options | select `$optionPref, * | sort `$sortPref, optionType | ft }"
-        } else {
+        }
+        else {
             $expr += " | select * -ExcludeProperty options | ft *"
         }
     }
