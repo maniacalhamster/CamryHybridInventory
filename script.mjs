17b3518b92e9a5c3d43ce9320c17c71cbeba5c0f
Akar Singh <aks012@ucsd.edu>
2024-04-01 09:24:38 -0700

    ugly wip for prompting user's location info

--- script.mjs
+++ script.mjs
@@ -1,18 +1,19 @@
 import puppeteer, { Page } from "puppeteer";
 import fs from 'fs';
+import readline from 'node:readline'
 
 /**
  * main body of the script I want to run
  * @param {Page} page
  */
 async function script(page) {
-    const inventory_url = "https://www.toyota.com/search-inventory/model/camryhybrid/?zipcode=91730"
+    const inventory_url = `https://www.toyota.com/search-inventory/model/camryhybrid/?zipcode=${zipcode}`
     const graphql_url = "https://api.search-inventory.toyota.com/graphql";
     const distance_sel = 'select[name="distance"]';
 
     await page.goto(inventory_url);
     await page.waitForSelector(distance_sel);
-    page.select(distance_sel, "100").then(() => console.log("set distance"));
+    page.select(distance_sel, radius).then(() => console.log(`set distance: ${radius}`));
 
     const vehicle_data = []
 
@@ -40,16 +41,46 @@ async function script(page) {
         }
     }
 
-    fs.writeFile('data.json', JSON.stringify(vehicle_data), (err) => console.log(err))
+    const _ = fs.writeFile('data.json', JSON.stringify(vehicle_data), (err) => console.log(err))
 }
 
-// "main" async function to call. Mainly scaffolding w/ minor configurations
-const browser = await puppeteer.launch({headless: false});
+/**
+ * "main" async function call 
+ * - prompts user for zipcode/radius if not set already
+ * - mostly puppeteer scaffolding w/ minor configurations
+ * - catches errors
+ * - defers a final close on the browser if it still exists
+ */
 async function main() {
-    const page = await browser.newPage();
-    page.setDefaultTimeout(5000);
-    await script(page);
+    console.log('Inventory data will be retrieved for dealers within a set radius around a chosen zipcode')
+    console.log('Please respond to the following prompts')
+
+    if (!zipcode || !radius) {
+        zipcode = await prompt('zipcode: ')
+        radius = await prompt('radius: ')
+    }
+
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage()
+    script(page)
+      .catch((err) => console.log(err))
+      .finally(() => {
+        browser?.close()
+        rl?.close()
+      });
 }
 
-// call "main", catching any errors and deferring a final close on browser
-main().catch(err => console.log(err)).finally(() => browser?.close())
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+let [zipcode, radius] = [0, 0]
+
+/**
+ * util function to transform node:readline question's callbacks into promises
+ * @param {string} query 
+ * @returns 
+ */
+const prompt = (query) => new Promise(resolve => rl.question(query, resolve))
+
+main()
